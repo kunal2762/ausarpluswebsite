d5553b4e9b86f74e328fcb32f236a34cd5249b0d
kunal2762 <kunalrahangdale2762@gmail.com>
2026-06-20 23:51:49 +0530

    Remove preloader on window load
    Removed preloader functionality from main.js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,14 +155,7 @@
     return false;
   });
 
-  // Preloader
-  $(window).on('load', function() {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
-        $(this).remove();
-      });
-    }
-  });
+ 
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
